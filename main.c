760d8a189034de6ba015ca254cefd7fd0e7f53da
Leonardo Thomaz <leonardo.thomaz@icompsoft.com.br>
2018-05-28 20:07:36 -0300

    feito merge de algumas alterações, sendo elaborado a parte do select para carregar cadastros

--- main.c
+++ main.c
@@ -35,7 +35,8 @@ void Imprimir();
     // `i_Telefone` VARCHAR(255) NOT NULL ); DECLARANDO A COLUNA "I_TELEFONE" DO TIPO VARCHAR 255 CARACTERES NÃO NULO
 
 int main (void){
-    //execu�ao
+    setlocale(LC_ALL, "Portuguese");
+    //execução
 
 
 
@@ -74,20 +75,23 @@ void Salvar()//salvar cadastros realizados, colocado aqui pra deixar pré carreg
             printf("Registros inseridos %d", mysql_affected_rows(&conexao));
         }
         else{
-            printf("Erro na insercao %d : %s", mysql_errno(&conexao), mysql_error(&conexao));
+            printf("Erro na inserção %d : %s", mysql_errno(&conexao), mysql_error(&conexao));
             mysql_close(&conexao);
         }
     }
     else{
-        printf("Falha de conexao");
+        printf("Falha de conexão");
         printf("Erro %d : %sn", mysql_errno(&conexao), mysql_error(&conexao));
     }
+    Limpar_Tela();
+    opcao();
 }
 
-void opcao()//op�ao para realizar um novo cadastro ou voltar ao menu inicial
+void opcao()//opção para realizar um novo cadastro ou voltar ao menu inicial
 {
+    setlocale(LC_ALL, "Portuguese");
     int l_op;
-    printf("Selecione a op��o desejada:\n1-Realizar novo Cadastro:\n2-Voltar ao menu principal:\n");
+    printf("Selecione a opção desejada:\n1-Realizar novo Cadastro:\n2-Voltar ao menu principal:\n");
         scanf("%d",&l_op);
         switch(l_op)
         {
@@ -101,25 +105,26 @@ void opcao()//op�ao para realizar um novo cadastro ou voltar ao menu inicial
     }
 }
 
-void Limpar_Tela()//limpar a tela a cada execu��o
+void Limpar_Tela()//limpar a tela a cada execução
 {
     system("cls");
 }
 
 void Cadastro_Cliente()//cadastrar um cliente
 {
+    setlocale(LC_ALL, "Portuguese");
     printf("Nome:\n");
-    scanf(" %s", i_Nome);
+    scanf(" %[^\n]s", i_Nome);
     printf("CPF:\n");
-    scanf(" %s", i_CPF);
+    scanf(" %[^\n]s", i_CPF);
     printf("Cidade:\n");
     scanf(" %[^\n]s", i_Cidade);
     printf("Veiculo:\n");
     scanf(" %s", i_Veiculo);
     printf("Data de Nascimento:\n");
-    scanf(" %s", i_DataNasc);
+    scanf(" %[^\n]s", i_DataNasc);
     printf("Telefone:\n");
-    scanf(" %s", i_Telefone);
+    scanf(" %[^\n]s", i_Telefone);
 
     printf("Deseja salvar este cadastro?\n S / N:\n");
     char l_Salvar,l_opcao;
@@ -133,29 +138,32 @@ void Cadastro_Cliente()//cadastrar um cliente
         opcao();
     }
 }
-void Orcamento()//imprimir or�amentos
+
+void Orcamento()//imprimir orçamentos
 {
+    char nome[50];
+    char veiculo[50];
     printf("Nome Cliente:\n");
-    scanf(" %s",i_Nome);
+    scanf(" %s",nome);
     printf("Veiculo:\n");
-    scanf(" %s",i_Veiculo);
-    printf("Informe quantidade de pe�as:\n");
+    scanf(" %s",veiculo);
+    printf("Informe quantidade de peças:\n");
     int i,peca;
     double valor=0,v, mao_de_obra;
     scanf("%d",&peca);
-    for(i=1;i<=peca;i++)
+    for(i=1; i<=peca; i++)
     {
-        printf("Pe�a %d valor R$:\n",i);
+        printf("Peça %d valor R$:\n",i);
         scanf("%lf",&v);
         valor+=v;
         printf("...............................................\n");
     }
-    printf("Valor M�o de obra R$:\n");
+    printf("Valor Mão de obra R$:\n");
     scanf("%lf",&mao_de_obra);
 
     valor+= mao_de_obra;
     printf("Total................................. R$%.2lf\n",valor);
-    printf("Deseja imprimir o or�amento? S/N\n");
+    printf("Deseja imprimir o orçamento? S/N\n");
     char op;
     scanf(" %c",&op);
     if(op=='s'||op=='S')
@@ -166,13 +174,37 @@ void Orcamento()//imprimir or�amentos
         Menu();
     }
 }
-void Imprimir()//iprimir o or�amento
+
+void Imprimir()//iprimir o orçamento
 {
 
 }
 void Mostrar_Cadastros()//mostrar todos os clientes cadastrados
 {
+    setlocale(LC_ALL, "Portuguese");
+    char Busca;
+    MYSQL conexao;
+    char query[200];
 
+    mysql_init(&conexao);
+    if(mysql_real_connect(&conexao, "127.0.0.1", "root", "", "APSALG2", 0, NULL, 0)){
+        printf("Conectado com sucesso!\n");
+        sprintf(query,"select * from apsalg2.clientes");
+        Busca = mysql_query(&conexao, query);
+        if (!mysql_query(&conexao, query)){
+            printf("%s", Busca);
+        }
+        else{
+            printf("Erro na inserção %d : %s", mysql_errno(&conexao), mysql_error(&conexao));
+            mysql_close(&conexao);
+        }
+    }
+    else{
+        printf("Falha de conexão");
+        printf("Erro %d : %sn", mysql_errno(&conexao), mysql_error(&conexao));
+    }
+    Limpar_Tela();
+    opcao();
 }
 void Apagar_Cadastros()//apagar todos os cadastros
 {
@@ -181,12 +213,13 @@ void Apagar_Cadastros()//apagar todos os cadastros
 
 void Menu()//menu principal
 {
+    setlocale(LC_ALL, "Portuguese");
     int escolha;
-    printf("Escolha uma op��o desejada;\n");
+    printf("Escolha uma opção desejada;\n");
     printf("1- Cadastro Cliente:\n");
     printf("2- Mostrar todos os cadastros:\n");
     printf("3- Apagar todos os cadastros;\n");
-    printf("4- Fazer or�amento:\n");
+    printf("4- Fazer orçamento:\n");
     printf("5- Sair:\n");
     scanf("%d",&escolha);
 
@@ -205,7 +238,7 @@ void Menu()//menu principal
     case (5):
         break;
 
-        /* printf("Favor selecionar uma op��o:")
+        /* printf("Favor selecionar uma opção:")
          Menu();*/
     }
 }
